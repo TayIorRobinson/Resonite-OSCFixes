cfe1ba5f9a40a85ebfc2c66236de98a702f66bb5
Taylor <tayyiorrobinson@gmail.com>
2025-12-15 17:32:01 +0000

    init

---
+++ OSCFixesMod.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Text.Json;
+using ResoniteModLoader;
+using System.Text.Json.Nodes;
+using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using FrooxEngine.Store;
+using HarmonyLib;
+using ResoniteHotReloadLib;
+using Rug.Osc;
+
+    
+namespace OSCFixes;
+
+
+
+public class OSCFixesMod : ResoniteMod {
+    internal const string VERSION_CONSTANT = "1.0.0"; 
+    public override string Name => "OSCFixes";
+    public override string Author => "U-TaylorRobinson";
+    public override string Version => VERSION_CONSTANT;
+    public override string Link => "https://robins.one";
+    private const string harmonyId = "one.robins.OSCFixes";
+    public override void OnEngineInit() {
+        Msg("Initialising!");
+        HotReloader.RegisterForHotReload(this);
+        Init();
+    }
+
+
+    static void OnHotReload(ResoniteMod mod)
+    {
+        Msg("Hot reloading!");
+        Init();
+    }
+
+
+    private static void Init() {
+        Msg("Init called");
+        Harmony harmony = new Harmony(harmonyId);
+        harmony.PatchAll();
+    }
+    
+    static void BeforeHotReload() {
+        Msg("Unloading!");
+        Harmony harmony = new Harmony(harmonyId);
+        harmony.UnpatchAll(harmonyId);
+    }
+    [HarmonyPatch]
+    class OSCPatch {
+        static MethodBase TargetMethod() {
+            Type oscReciever = typeof(OSC_Receiver);
+            Msg("OSC Reciever Class " + oscReciever);
+            return oscReciever.GetMethod("ReceiveData", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        
+        static bool Prefix(
+            ref OSC_Receiver __instance,
+            OscReceiver receiver
+        ) {
+            var atEntry = receiver.State;
+            Msg("Entering OSC_Reciever#RecieveData with state " + atEntry + " on " +__instance.ReferenceID);
+            while (receiver.State != OscSocketState.Connected) {
+                Msg("Waiting for OSC Connected on " + __instance.ReferenceID);
+                Thread.Sleep(10);
+            }
+            return true;
+        }
+        static void Postfix(ref OSC_Receiver __instance)
+        {
+            Msg("Leaving OSC_Reciever#RecieveData on " + __instance.ReferenceID);
+        }
+    }
+}
